3780d95f30c5602c63f7115787c5a33f4f7f7e01
ducnm <mducbg2000@gmail.com>
2022-12-08 17:46:48 +0700

    Add fed_adam, fed_yogi

--- utils.py
+++ utils.py
@@ -5,9 +5,16 @@ from typing import Any, List, Tuple
 from functools import reduce
 
 from sklearn.feature_selection import SelectKBest, f_regression
+from sklearn.model_selection import train_test_split
+from flwr.server.strategy import FedAvg
+
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Dropout, Bidirectional
+from keras.metrics import MeanSquaredError
 
 NDArray = NDArray[Any]
 NDArrays = List[NDArray]
+LSTM_MODEL = Sequential
 
 
 def read_raw_dataset(url: str, index_col: str, target_name: str, drop: List[str] = []):
@@ -21,11 +28,34 @@ def read_raw_dataset(url: str, index_col: str, target_name: str, drop: List[str]
 def select_features(X: pd.DataFrame, y: pd.Series, k=10):
     selector = SelectKBest(score_func=f_regression, k=k)
     selector.fit(X, y)
-    return X[X.columns[selector.get_support(indices=True)]], selector
+    return X[X.columns[selector.get_support(indices=True)]]
 
 
+def read_chunk_and_split(id: int):
+    X_raw, y = read_raw_dataset(
+        "datasets/chunk{}.csv".format(id), "ID", "VMOS")
+    X = select_features(X_raw, y)
+    return train_test_split(X, y, train_size=0.8)
 
-def aggregate(results: List[Tuple[NDArrays, int]]) -> NDArrays:
+
+def build_model(number_of_feature) -> Sequential:
+    model = Sequential()
+    model.add(Bidirectional(LSTM(128, return_sequences=False),
+              input_shape=(number_of_feature, 1)))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.2))
+    model.add(Dense(64))
+    model.add(Dropout(0.2))
+    model.add(Dense(32))
+    model.add(Dropout(0.2))
+    model.add(Dense(1, activation='linear'))
+
+    model.compile(optimizer='adam',
+                  loss='mse', metrics=[MeanSquaredError()])
+    return model
+
+
+def fed_avg(results: List[Tuple[NDArrays, int]]) -> NDArrays:
     """Compute weighted average."""
     # Calculate the total number of examples used during training
     num_examples_total = sum([num_examples for _, num_examples in results])
