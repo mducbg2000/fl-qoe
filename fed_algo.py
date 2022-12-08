4ab59b95eba034907ffa0c23c5f4eddadcaec6a5
ducnm <mducbg2000@gmail.com>
2022-12-08 17:48:14 +0700

    Add fed_adam, fed_yogi

---
+++ fed_algo.py
@@ -0,0 +1,94 @@
+from dataclasses import dataclass
+from numpy.typing import NDArray
+from functools import reduce
+from typing import Any, List, Optional, Tuple
+import numpy as np
+
+NDArray = NDArray[Any]
+NDArrays = List[NDArray]
+
+
+@dataclass(frozen=True)
+class ClientParam:
+    layers: NDArrays
+    num_examples: int
+
+
+def fed_avg(clients: List[ClientParam]) -> NDArrays:
+    """Compute weighted average."""
+    # Calculate the total number of examples used during training
+    num_examples_total = sum([client.num_examples for client in clients])
+
+    # Create a list of weights, each multiplied by the related number of examples
+    weighted_weights = [
+        [layer * client.num_examples for layer in client.layers] for client in clients
+    ]
+
+    # Compute average weights of each layer
+    weights_prime: NDArrays = [
+        reduce(np.add, layer_updates) / num_examples_total
+        for layer_updates in zip(*weighted_weights)
+    ]
+    return weights_prime
+
+
+def fed_adam(
+    clients: List[ClientParam],
+    old_global_weights: NDArrays,
+    old_m_t: Optional[NDArrays] = None,
+    old_v_t: Optional[NDArrays] = None
+):
+    fedavg_params_aggregated: NDArrays = fed_avg(clients)
+    delta_t: NDArrays = [
+        x-y for x, y in zip(fedavg_params_aggregated, old_global_weights)
+    ]
+
+    m_t = old_m_t or [np.zeros_like(x) for x in delta_t]
+    new_m_t = [
+        np.multiply(0.9, x) + 0.1*y for x, y in zip(m_t, delta_t)
+    ]
+
+    v_t = old_v_t or [np.zeros_like(x) for x in delta_t]
+    new_v_t = [
+        0.99*x + 0.01*np.multiply(y, y) for x, y in zip(v_t, delta_t)
+    ]
+
+    new_weights = [
+        x + (1e-1)*y / (np.sqrt(z) + (1e-9))
+        for x, y, z in zip(old_global_weights, new_m_t, new_v_t)
+    ]
+
+    return new_weights, new_m_t, new_v_t
+
+
+def fed_yogi(
+    clients: List[ClientParam],
+    old_global_weights: NDArrays,
+    old_m_t: Optional[NDArrays] = None,
+    old_v_t: Optional[NDArrays] = None
+):
+    fedavg_params_aggregated: NDArrays = fed_avg(clients)
+
+    delta_t: NDArrays = [
+        x-y for x, y in zip(fedavg_params_aggregated, old_global_weights)
+    ]
+
+    m_t = old_m_t or [np.zeros_like(x) for x in delta_t]
+    new_m_t = [
+        np.multiply(0.9, x) + 0.1*y
+        for x, y in zip(m_t, delta_t)
+    ]
+
+    v_t = old_v_t or [np.zeros_like(x) for x in delta_t]
+    new_v_t = [
+        x - 0.01*np.multiply(y, y)*np.sign(x-np.multiply(y, y))
+        for x, y in zip(v_t, delta_t)
+    ]
+
+    new_weights = [
+        x + (1e-1)*y / (np.sqrt(z) + (1e-9))
+        for x, y, z in zip(old_global_weights, new_m_t, new_v_t)
+    ]
+
+    return new_weights, new_m_t, new_v_t
+
---
+++ test-lstm.py
@@ -0,0 +1,52 @@
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+from utils import select_features, build_model, NDArrays, LSTM_MODEL, read_raw_dataset, fed_avg
+
+TRAIN_RESULT = tuple[LSTM_MODEL, int]
+AGGREGATE_PARAM = tuple[NDArrays, int]
+NUMBER_OF_FEATURES = 10
+
+
+def read_chunk(id: int) -> tuple[pd.DataFrame, pd.Series]:
+    X_raw, y = read_raw_dataset(
+        "datasets/pokemon_users/chunk{}.csv".format(id), "id", "MOS", ["user_id"])
+    X = select_features(X_raw, y, k=NUMBER_OF_FEATURES)
+    return X, y
+
+
+def train(id: int, init_weights: NDArrays) -> TRAIN_RESULT:
+    X_train, y_train = read_chunk(id)
+    lstm = build_model(NUMBER_OF_FEATURES)
+    lstm.set_weights(init_weights)
+    lstm.fit(X_train, y_train, validation_split=0.05,
+             batch_size=128, epochs=50, shuffle=True)
+    return lstm, len(X_train)
+
+
+def compute_loss(model: LSTM_MODEL, X_test, y_test) -> float:
+    y_pred = model.predict(X_test)
+    return mean_squared_error(y_test, y_pred)
+
+
+if __name__ == "__main__":
+    global_model: LSTM_MODEL = build_model(NUMBER_OF_FEATURES)
+    init_weights = global_model.get_weights()
+
+    train_results = [train(id+1, init_weights) for id in range(180)]
+
+    global_weights = fed_avg(
+        [(result[0].get_weights(), result[1]) for result in train_results])
+
+    global_model.set_weights(global_weights)
+
+    X_test, y_test = read_chunk(181)
+
+    local_mse = [compute_loss(model, X_test, y_test)
+                 for (model, _) in train_results]
+
+    global_mse = compute_loss(global_model, X_test, y_test)
+
+    print("Local mse: \n")
+    print(local_mse)
+    print("Global mse: \n")
+    print(global_mse)
