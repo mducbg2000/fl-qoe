de3b73718190cb42f2913eaa875e4bbe3790fe8f
ducnm <mducbg2000@gmail.com>
2022-12-14 22:40:47 +0700

    Compare dl models

--- fed_algo.py
+++ fed_algo.py
@@ -11,6 +11,19 @@ Layers = List[NDArray]
 ClientParam = Tuple[Layers, int]
 
 
+def fed_avg(clients: List[ClientParam]) -> Layers:
+    num_examples_total = sum([num_examples for _, num_examples in clients])
+
+    weighted_weights = [[layer * num_examples for layer in layers]
+                        for layers, num_examples in clients]
+
+    weights_prime: Layers = [
+        reduce(np.add, layer_updates) / num_examples_total
+        for layer_updates in zip(*weighted_weights)
+    ]
+    return weights_prime
+
+
 class FedAlgo(ABC):
 
     @abstractmethod
